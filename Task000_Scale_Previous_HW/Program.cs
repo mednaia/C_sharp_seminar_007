eb6325b5e9db1020792bc815ad9987467e877d6d
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-03 22:56:12 +0300

    Task 000 Scaling of figure

--- Task000_Scale_Previous_HW/Program.cs
+++ Task000_Scale_Previous_HW/Program.cs
@@ -5,11 +5,11 @@
 В результате показать координаты, которые получатся.
 при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)"*/
 
-int CountSpace(string str)
+int CountSymbol(string str, char symbol)
 {
     int count =0;
     for(int i=0;i<str.Length;i++)
-        if(str[i]==' ')
+        if(str[i]==symbol)
             count++;
     return count;
 }
@@ -23,13 +23,13 @@ int CountSpace(string str)
 //     return count;
 // }
 
-string[] Split(string str)
+string[] Split(string str, char symbol)
 {
-    string[] coordinates = new string[CountSpace(str)+1];
+    string[] coordinates = new string[CountSymbol(str,symbol)+1];
     int amount =0;
     for(int i=0;i<str.Length;i++)
     {
-        while(i<str.Length && str[i]!=' ')
+        while(i<str.Length && str[i]!=symbol)
         {
             coordinates[amount]+=str[i];
             i++;
@@ -38,10 +38,37 @@ string[] Split(string str)
     }
     return coordinates;
 }
-void PrintArray(string[] array)
+// void PrintArray(string[] array)
+// {
+//     for(int i=0; i<array.Length; i++)
+//         Console.WriteLine(array[i]);
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for(int i=0;i<matrix.GetLength(0);i++)
+//         for(int j=0;j<matrix.GetLength(1);j++)
+//             Console.Write($"{matrix[i,j]} ");
+//     Console.WriteLine();
+// }
+string Substring(string str, int startIndex, int lastIndex)
 {
-    for(int i=0; i<array.Length; i++)
-        Console.WriteLine(array[i]);
+    string result="";
+    for(int i=startIndex;i<lastIndex;i++)
+        result+=str[i];
+    return result;
+}
+int[,] ParsePoints(string[] coordinates)
+{
+    int[,] points = new int[coordinates.Length,2];
+    for(int i=0;i<coordinates.Length;i++)
+    {
+        string substring = Substring(coordinates[i],1,coordinates[i].Length-1); // string substring = coordinates[1].Substring(1,coordinates[i].Length-1)
+        string[] pointCoordinates = Split(substring, ',');
+        //Console.WriteLine($"{pointCoordinates[0]} {pointCoordinates[1]}");
+        for(int j=0;j<pointCoordinates.Length;j++) //string[] pointCoordinates = substring.Split(',');
+            points[i,j] = int.Parse(pointCoordinates[j]);
+    }
+    return points;
 }
 Console.Write("Enter coordinates of figure: ");
 string input = Console.ReadLine() ?? "";
@@ -50,5 +77,11 @@ if(input==null || input.Length==0)
     Console.WriteLine("Enter not empty string");
     return;
 }
-string[] coordinates = Split(input);
-PrintArray(coordinates);
+Console.WriteLine("Enter coefficient of scaling: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+string[] coordinates = Split(input,' ');
+// PrintArray(coordinates);
+int[,] points = ParsePoints(coordinates); // string[] coordinates = input.Split(' ');
+// PrintMatrix(points);
+for(int i=0;i<points.GetLength(0);i++)
+    Console.Write($"({points[i,0]*k}, {points[i,1]*k}) ");
